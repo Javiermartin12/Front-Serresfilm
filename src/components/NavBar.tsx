ac6a9c1093a887d4078c37acfe33ff677894a303
javiermartinruiz <javiermartinruiz21@gmail.com>
2025-02-06 18:17:37 +0100

    Modified buttons and styles

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,9 +4,6 @@ import {
   Button,
   Drawer,
   IconButton,
-  List,
-  ListItem,
-  ListItemText,
   Toolbar,
   Typography,
   Box,
@@ -15,14 +12,14 @@ import Link from "next/link";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useState } from "react";
 
-const navLinks = [
-  { title: "Home", path: "/" },
-  { title: "About", path: "/aboutUs" },
-  { title: "Contact", path: "/contact" },
-  { title: "Join Us", path: "/joinUs" },
-  { title: "Greenhouse", path: "/greenhouse" },
-  { title: "Warehouse", path: "/warehouse" },
-];
+// const navLinks = [
+//   { title: "Home", path: "/" },
+//   { title: "About", path: "/aboutUs" },
+//   { title: "Contact", path: "/contact" },
+//   { title: "Join Us", path: "/joinUs" },
+//   { title: "Greenhouse", path: "/greenhouse" },
+//   { title: "Warehouse", path: "/warehouse" },
+// ];
 
 function NavBar() {
   const [open, setOpen] = useState<boolean>(false);
@@ -41,83 +38,124 @@ function NavBar() {
   return (
     <>
       {/* Navbar principal */}
-      <AppBar position="fixed" color="secondary">
-        <Toolbar sx={{ display: "flex", borderRadius: 1 }}>
-          {/* Menú hamburguesa en móviles */}
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ display: { xs: "block", md: "none" } }}
-            onClick={toggleDrawer(true)}
-          >
-            <MenuIcon />
-          </IconButton>
+      <Box sx={{ display: "flex", justifyContent: "center", margin: 1 }}>
+        <AppBar
+          position="sticky"
+          sx={{
+            width: "80%",
+            backgroundColor: "rgba(255, 255, 255, 0.5)",
+            border: "1px solid #ccc",
+            borderRadius: 2,
+          }}
+        >
+          <Toolbar sx={{ display: "flex" }}>
+            {/* Menú hamburguesa en móviles */}
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{
+                display: {
+                  xs: "block",
+                  md: "none",
+                },
+              }}
+              onClick={toggleDrawer(true)}
+            >
+              <MenuIcon />
+            </IconButton>
 
-          {/* Título */}
-          <Typography
-            variant="h6"
-            sx={{
-              flexGrow: 1,
-              textAlign: "center",
-              display: {
-                xs: "block",
-                sm: "block",
-                md: "none",
-                lg: "none",
-                xl: "none",
-              },
-            }}
-          >
-            Serrefilm
-          </Typography>
+            {/* Título */}
+            <Typography
+              variant="h6"
+              component={Link}
+              href="/"
+              width={1}
+              sx={{
+                flexGrow: 1,
+                textShadow: "1px 5px 10px rgba(0, 0, 0, 0.5)",
+                textAlign: "center",
+                display: {
+                  xs: "block",
+                  sm: "block",
+                  md: "none",
+                  lg: "none",
+                  xl: "none",
+                },
+              }}
+            >
+              Serrefilm
+            </Typography>
 
-          {/* Links en escritorio */}
+            {/* Barra en escritorio */}
+            <Box
+              sx={{
+                textAlign: "center",
+                alignItems: "center",
+                justifyContent: "center",
+                display: { xs: "none", md: "flex" },
+              }}
+            >
+              <Button component={Link} href="/" color="inherit">
+                Home
+              </Button>
+              <Button component={Link} href="/aboutUs" color="inherit">
+                About
+              </Button>
+              <Button component={Link} href="/contact" color="inherit">
+                Contact
+              </Button>
+              <Button component={Link} href="/joinUs" color="inherit">
+                Join Us
+              </Button>
+              <Button component={Link} href="/greenhouse" color="inherit">
+                Greenhouse
+              </Button>
+              <Button component={Link} href="/warehouse" color="inherit">
+                Warehouse
+              </Button>
+            </Box>
+          </Toolbar>
+        </AppBar>
+
+        {/* Drawer (Menú lateral en móviles) */}
+        <Drawer
+          anchor="left"
+          PaperProps={{
+            sx: { width: 250, backgroundColor: "#dedede" },
+          }}
+          open={open}
+          onClose={toggleDrawer(false)}
+        >
           <Box
             sx={{
-              textAlign: "center",
-              alignItems: "center",
-              justifyContent: "center",
-              display: { xs: "none", md: "flex" },
+              display: "flex",
+              flexDirection: "column",
+              padding: 3,
+              gap: 2,
             }}
           >
-            {navLinks.map((item) => (
-              <Button
-                sx={{ textAlign: "center" }}
-                key={item.path}
-                color="inherit"
-                component={Link}
-                href={item.path}
-              >
-                {item.title}
-              </Button>
-            ))}
+            <Button component={Link} href="/" color="inherit">
+              Home
+            </Button>
+            <Button component={Link} href="/aboutUs" color="inherit">
+              About
+            </Button>
+            <Button component={Link} href="/contact" color="inherit">
+              Contact
+            </Button>
+            <Button component={Link} href="/joinUs" color="inherit">
+              Join Us
+            </Button>
+            <Button component={Link} href="/greenhouse" color="inherit">
+              Greenhouse
+            </Button>
+            <Button component={Link} href="/warehouse" color="inherit">
+              Warehouse
+            </Button>
           </Box>
-        </Toolbar>
-      </AppBar>
-
-      {/* Drawer (Menú lateral en móviles) */}
-      <Drawer
-        anchor="left"
-        PaperProps={{
-          sx: { width: 250, backgroundColor: "#dedede", color: "white" },
-        }}
-        open={open}
-        onClose={toggleDrawer(false)}
-      >
-        <List>
-          {navLinks.map((item) => (
-            <ListItem
-              key={item.path}
-              component={Link}
-              href={item.path}
-              onClick={toggleDrawer(false)}
-            >
-              <ListItemText primary={item.title} />
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
+        </Drawer>
+      </Box>
     </>
   );
 }
