393b8c3401ac60a5bd37480843f25ec7ebb630c3
javiermartinruiz <javiermartinruiz21@gmail.com>
2025-02-07 10:13:35 +0100

    Restarted component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import { Box } from "@mui/material";
-
 export default function Home() {
-  return (
-    <Box
-      sx={{ display: "flex", flexDirection: "column", height: "100vh" }}
-    ></Box>
-  );
+  return;
 }
