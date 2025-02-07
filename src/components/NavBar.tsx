f631c438521f39a691e779c93abb6ed74be20a39
javiermartinruiz <javiermartinruiz21@gmail.com>
2025-02-07 12:34:49 +0100

    Modified style to do responsive.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -38,101 +38,63 @@ function NavBar() {
   return (
     <>
       {/* Navbar principal */}
-      <Box sx={{ display: "flex", justifyContent: "center", margin: 1 }}>
-        <AppBar
-          position="sticky"
-          sx={{
-            width: "80%",
-            backgroundColor: "rgba(255, 255, 255, 0.5)",
-            border: "1px solid #ccc",
-            borderRadius: 2,
-          }}
-        >
-          <Toolbar sx={{ display: "flex" }}>
-            {/* Menú hamburguesa en móviles */}
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{
-                display: {
-                  xs: "block",
-                  md: "none",
-                },
-              }}
-              onClick={toggleDrawer(true)}
-            >
-              <MenuIcon />
-            </IconButton>
 
-            {/* Título */}
-            <Typography
-              variant="h6"
-              component={Link}
-              href="/"
-              width={1}
-              sx={{
-                flexGrow: 1,
-                textShadow: "1px 5px 10px rgba(0, 0, 0, 0.5)",
-                textAlign: "center",
-                display: {
-                  xs: "block",
-                  sm: "block",
-                  md: "none",
-                  lg: "none",
-                  xl: "none",
-                },
-              }}
-            >
-              Serrefilm
-            </Typography>
+      <AppBar
+        sx={{
+          width: { xs: "90%", sm: "80%", md: "80%", lg: "80%", xl: "80%" },
+          top: "5%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          backgroundColor: "rgba(255, 255, 255, 0.5)",
+          border: "1px solid #ccc",
+          borderRadius: 2,
+        }}
+      >
+        <Toolbar sx={{ display: "flex" }}>
+          {/* Menú hamburguesa en móviles */}
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{
+              display: {
+                xs: "block",
+                md: "none",
+              },
+            }}
+            onClick={toggleDrawer(true)}
+          >
+            <MenuIcon />
+          </IconButton>
 
-            {/* Barra en escritorio */}
-            <Box
-              sx={{
-                textAlign: "center",
-                alignItems: "center",
-                justifyContent: "center",
-                display: { xs: "none", md: "flex" },
-              }}
-            >
-              <Button component={Link} href="/" color="inherit">
-                Home
-              </Button>
-              <Button component={Link} href="/aboutUs" color="inherit">
-                About
-              </Button>
-              <Button component={Link} href="/contact" color="inherit">
-                Contact
-              </Button>
-              <Button component={Link} href="/joinUs" color="inherit">
-                Join Us
-              </Button>
-              <Button component={Link} href="/greenhouse" color="inherit">
-                Greenhouse
-              </Button>
-              <Button component={Link} href="/warehouse" color="inherit">
-                Warehouse
-              </Button>
-            </Box>
-          </Toolbar>
-        </AppBar>
+          {/* Título */}
+          <Typography
+            variant="h6"
+            component={Link}
+            href="/"
+            width={1}
+            sx={{
+              flexGrow: 1,
+              textShadow: "1px 5px 10px rgba(0, 0, 0, 0.5)",
+              textAlign: "center",
+              display: {
+                xs: "block",
+                sm: "block",
+                md: "none",
+                lg: "none",
+                xl: "none",
+              },
+            }}
+          >
+            Serrefilm
+          </Typography>
 
-        {/* Drawer (Menú lateral en móviles) */}
-        <Drawer
-          anchor="left"
-          PaperProps={{
-            sx: { width: 250, backgroundColor: "#dedede" },
-          }}
-          open={open}
-          onClose={toggleDrawer(false)}
-        >
+          {/* Barra en escritorio */}
           <Box
             sx={{
-              display: "flex",
-              flexDirection: "column",
-              padding: 3,
-              gap: 2,
+              textShadow: "2px 2px 4px rgba(0, 0, 0, 0.7)",
+
+              display: { xs: "none", md: "block" },
             }}
           >
             <Button component={Link} href="/" color="inherit">
@@ -154,8 +116,46 @@ function NavBar() {
               Warehouse
             </Button>
           </Box>
-        </Drawer>
-      </Box>
+        </Toolbar>
+      </AppBar>
+
+      {/* Drawer (Menú lateral en móviles) */}
+      <Drawer
+        anchor="left"
+        PaperProps={{
+          sx: { width: 250, backgroundColor: "#dedede" },
+        }}
+        open={open}
+        onClose={toggleDrawer(false)}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            padding: 3,
+            gap: 2,
+          }}
+        >
+          <Button component={Link} href="/" color="inherit">
+            Home
+          </Button>
+          <Button component={Link} href="/aboutUs" color="inherit">
+            About
+          </Button>
+          <Button component={Link} href="/contact" color="inherit">
+            Contact
+          </Button>
+          <Button component={Link} href="/joinUs" color="inherit">
+            Join Us
+          </Button>
+          <Button component={Link} href="/greenhouse" color="inherit">
+            Greenhouse
+          </Button>
+          <Button component={Link} href="/warehouse" color="inherit">
+            Warehouse
+          </Button>
+        </Box>
+      </Drawer>
     </>
   );
 }
