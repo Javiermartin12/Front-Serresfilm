08c58e7ba1f3f787f505e38b83e6e246e0e4d4d7
javiermartinruiz <javiermartinruiz21@gmail.com>
2025-02-17 16:05:20 +0100

    Relocated component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import BackGroundPrincipal from "@/components/BackGroundPrincipal";
-import NavBar from "@/components/NavBar";
-import ReviewCard from "@/components/ReviewCard";
+
 import ReviewCarousel from "@/components/ReviewCarousel";
 import theme from "@/styles/theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
@@ -16,15 +15,8 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <ThemeProvider theme={theme}>
-          <NavBar />
           <BackGroundPrincipal />
 
-          <ReviewCard
-            avatarUrl="https://randomuser.me/api/portraits/men/1.jpg"
-            name="Javier Martín"
-            rating={2}
-            comment="Me encantó el servicio, volveré a comprar."
-          />
           <ReviewCarousel />
           <CssBaseline></CssBaseline>
           {children}
