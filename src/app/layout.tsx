969781fccfa6b0b1ebbd6ecb0940a4b4d3bb68ba
javiermartinruiz <javiermartinruiz21@gmail.com>
2025-02-17 17:58:24 +0100

    Changed structure from the porject

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-import BackGroundPrincipal from "@/components/BackGroundPrincipal";
-
-import ReviewCarousel from "@/components/ReviewCarousel";
 import theme from "@/styles/theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 
@@ -15,9 +12,6 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <ThemeProvider theme={theme}>
-          <BackGroundPrincipal />
-
-          <ReviewCarousel />
           <CssBaseline></CssBaseline>
           {children}
         </ThemeProvider>
