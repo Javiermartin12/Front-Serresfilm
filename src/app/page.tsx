eaf286e135cb408adddb711fb8bfa0e11a6d8073
javiermartinruiz <javiermartinruiz21@gmail.com>
2025-02-17 17:58:40 +0100

    Imported component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+"use client";
+import BackGroundPrincipal from "@/components/BackGroundPrincipal";
+import Footer from "@/components/Footer";
+import GreenhouseHome from "@/components/GreenhouseHome";
+import ReviewCarousel from "@/components/ReviewCarousel";
+import WarehouseHome from "@/components/WarehouseHome";
+
 export default function Home() {
-  return;
+  return (
+    <>
+      <BackGroundPrincipal />
+      <GreenhouseHome />
+      <WarehouseHome />
+      <ReviewCarousel />
+      <Footer />
+    </>
+  );
 }
