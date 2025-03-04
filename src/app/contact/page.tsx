bfbce596aaec14f755844d0c60dd5d20f532e6b8
javiermartinruiz <javiermartinruiz21@gmail.com>
2025-03-04 18:17:26 +0100

    Chnaged name of component

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,14 @@
+import Footer from "@/components/Footer";
+import NavBar from "@/components/NavBar";
+
+export default function ContactPage() {
+  return (
+    <div>
+      <NavBar />
+      <h1>Contacto</h1>
+
+      <p>Puedes contactarnos en este formulario.</p>
+      <Footer />
+    </div>
+  );
+}
---
+++ src/app/joinUs/page.tsx
@@ -0,0 +1,14 @@
+import Footer from "@/components/Footer";
+import NavBar from "@/components/NavBar";
+
+function joinUs() {
+  return (
+    <div>
+      <NavBar />
+      <h1>Join us</h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default joinUs;
