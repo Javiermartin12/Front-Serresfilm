e921e925f9f60552f0ee53ccecb595bed8b89729
javiermartinruiz <javiermartinruiz21@gmail.com>
2025-03-26 18:00:50 +0100

    Fixed

--- src/app/greenHouse/page.tsx
+++ src/app/greenHouse/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 import Footer from "@/components/Footer";
 
-import BenefitsGreenhouse from "@/components/GreenHouse/BenefitsGreenhouse";
-import GetGreenhouse from "@/components/GreenHouse/GetGreenhouse";
-import GreenHouseHeader from "@/components/GreenHouse/GreenHouseHeader";
-import InformationGreenHouse from "@/components/GreenHouse/InformationGreenHouse";
-import ServicesGreenhouse from "@/components/GreenHouse/ServicesGreenhouse";
+import BenefitsGreenhouse from "../../components/GreenHouse/BenefitsGreenhouse";
+import GetGreenhouse from "../../components/GreenHouse/GetGreenhouse";
+import GreenHouseHeader from "../../components/GreenHouse/GreenHouseHeader";
+import InformationGreenHouse from "../../components/GreenHouse/InformationGreenHouse";
+import ServicesGreenhouse from "../../components/GreenHouse/ServicesGreenhouse";
 
 import NavBar from "@/components/NavBar";
 
