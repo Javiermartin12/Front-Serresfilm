e900a5ee04e33f349cff1d12aef926f741bab3f6
javiermartinruiz <javiermartinruiz21@gmail.com>
2025-05-06 15:54:54 +0200

    Deleted compnents

--- src/app/machinery/page.tsx
+++ src/app/machinery/page.tsx
@@ -1,12 +1,9 @@
 "use client";
 import Footer from "@/components/Footer";
 import CallToAction from "@/components/Machinery/CallToAction";
-
-import InformationMachinery from "@/components/Machinery/InformationMachinery";
 import MachineryHeader from "../../components/Machinery/MachineryHeader";
 import Products from "../../components/Machinery/Products";
 
-import { VideosMachinery } from "@/components/Machinery/VideosMachinery";
 import NavBar from "@/components/NavBar";
 
 import React from "react";
@@ -17,8 +14,6 @@ function machinery() {
       <NavBar />
       <MachineryHeader />
       <Products />
-      <InformationMachinery />
-      <VideosMachinery />
       <CallToAction />
       <Footer />
     </div>
