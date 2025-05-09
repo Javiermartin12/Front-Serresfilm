02b26c1fa91412111d21175b2f90e6f4f508b019
javiermartinruiz <javiermartinruiz21@gmail.com>
2025-05-09 17:56:37 +0200

    Modified components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,15 @@ import Footer from "@/components/Footer";
 import GreenhouseHome from "@/components/Home/GreenhouseHome";
 import HeaderHome from "@/components/Home/HeaderHome";
 import MachineryHome from "@/components/Home/MachineryHome";
-import ReviewCarousel from "@/components/Home/ReviewCarousel";
+// import ReviewCarousel from "@/components/Home/ReviewCarousel";
 import WarehouseHome from "@/components/Home/WarehouseHome";
-
 
 export default function Home() {
   return (
     <>
       <HeaderHome />
       <GreenhouseHome />
-      <ReviewCarousel />
+      {/* <ReviewCarousel /> */}
       <WarehouseHome />
       <MachineryHome />
       <Footer />
